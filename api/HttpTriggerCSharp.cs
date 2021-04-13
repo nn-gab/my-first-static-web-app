bb62a1119ab46990355e597b1728ad0971640b16
Gabriele Pividori <gpividori@nanthealth.com>
2021-04-13 15:02:17 +0100

    code

---
+++ api/HttpTriggerCSharp.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class GetMessage
+    {
+        [FunctionName("GetMessage")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "message")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult(new {
+                Text = "Hello from the API"
+            });
+        }
+    }
+}
